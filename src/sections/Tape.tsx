fc8c11cb3a2da2845658d3ffe3a4141f0a1ca2ce
Shima Safari <shimasafarimg@gmail.com>
2025-05-11 00:33:09 +0330

    add Animations in Tape

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,4 +1,5 @@
 import { Smile } from "lucide-react";
+import { Fragment } from "react";
 export const TapeSection = () => {
   const words = [
     "Performant",
@@ -16,15 +17,19 @@ export const TapeSection = () => {
   return (
     <div className="py-16 lg:py-24 overflow-x-clip">
       <div className="bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3 -mx-1">
-        <div className="flex mask-x-from-90%">
-          <div className="flex flex-none gap-4 py-3">
-            {words.map((word) => (
-              <div key={word} className="inline-flex gap-4 items-center">
-                <span className="text-gray-900 uppercase font-extrabold text-sm">
-                  {word}
-                </span>
-                <Smile className="size-6 -rotate-12 text-gray-900" />
-              </div>
+        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex flex-none gap-4 pr-4 py-3 animate-move-left [animation-duration:30s]">
+            {[...new Array(2)].fill(0).map((_, idx) => (
+              <Fragment key={idx}>
+                {words.map((word) => (
+                  <div key={word} className="inline-flex gap-4 items-center">
+                    <span className="text-gray-900 uppercase font-extrabold text-sm">
+                      {word}
+                    </span>
+                    <Smile className="size-6 -rotate-12 text-gray-900" />
+                  </div>
+                ))}
+              </Fragment>
             ))}
           </div>
         </div>
